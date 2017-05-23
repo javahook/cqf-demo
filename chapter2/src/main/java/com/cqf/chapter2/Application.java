2f1c2d2257db79b4c44b83094085a5a4d16f2820
cqf <chen_qifu@163.com>
2017-05-23 15:05:22 +0800

    Spring-Boot 快速入门——在tomcat容器中部署

--- chapter2/src/main/java/com/cqf/chapter2/Application.java
+++ chapter2/src/main/java/com/cqf/chapter2/Application.java
@@ -15,7 +15,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @Controller
 @SpringBootApplication
 public class Application {
-    
+     
 	public static void main(String[] args) {
 
 		SpringApplication.run(Application.class, args);
